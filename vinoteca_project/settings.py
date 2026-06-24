68a79b02c1aee7a143ad1cd5ec19b021e2d6c5da
Sol Ailen Kalapuj <skalapuj@gmail.com>
2026-06-24 15:20:52 -0300

    Corrección host permitido

--- vinoteca_project/settings.py
+++ vinoteca_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(f3#+2w%kakbhyyrqgrbdpzopf4$kyfkwqqp1b_j@!52%%ben&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pwii-proyectovinoteca.onrender.com/']
+ALLOWED_HOSTS = ['pwii-proyectovinoteca.onrender.com']
 
 
 # Application definition
